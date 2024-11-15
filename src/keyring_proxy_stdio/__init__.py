90fefc6e806bb173c70f69547aa5a7036ee35043
Kalle M. Aagaard <km@conscia.com>
2024-11-15 08:18:22 +0100

    devrel

--- src/keyring_proxy_stdio/__init__.py
+++ src/keyring_proxy_stdio/__init__.py
@@ -1,19 +1,19 @@
 from typing import override
 
 from jaraco.classes import properties
-from keyring_proxy.stdioproxy import DEFAULT_EXE_PATH, RuntimeTransport
-from keyring_proxy.transport import ProxyBackend, TransportClient
+from keyring_proxy.backend import ProxyBackend
+from keyring_proxy.stdioproxy import DEFAULT_EXE_PATH, StdioClient
+from keyring_proxy.transport import TransportClient
 
 PRIORITY = 9.9
 
 
 class StdioProxyBackend(ProxyBackend):
-
-    exe = DEFAULT_EXE_PATH
+    exe = "C:\\Users\\km\\Projects\\github\\py-keyring-proxy-cli\\.venv\\Scripts\\keyring-proxy.exe"
 
     @override
     def _get_transport(self) -> TransportClient:
-        return RuntimeTransport.from_path(self.exe)
+        return StdioClient.from_path(self.exe)
 
     @properties.classproperty
     def priority(cls):
